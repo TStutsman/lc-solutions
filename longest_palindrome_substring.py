b9d17b2bac70564b2af266e23a055ae08c78281e
TStutsman <teagandstutsman@gmail.com>
2024-06-03 16:17:48 -0400

    longest_palindrome_bfs_timeout

--- longest_palindrome_substring.py
+++ longest_palindrome_substring.py
@@ -1,7 +1,8 @@
-# left and right pointers pointing to start, end indeces of string s
+        # left and right pointers pointing to start, end indeces of string s
         # check if we have visited left,right already
         # check if substring[left:right] is palindrome
-        # if it is compare to longest and save maximum
+        # if it is, it is the longest possible palindrome, so return it
+        # if not:
         # add left,right to visited
         # increment left and recur
         # decrement right and recur
@@ -21,7 +22,7 @@
         
         # def recursively_check_substrings(left, right, longest):
         #     # if we already visited don't check or recur
-        #     if (left, right) in visited: return
+        #     if (left, right) in visited or left >= right: return
             
         #     visited.add((left,right))
         #     substring = s[left:right]
@@ -34,4 +35,76 @@
 
         # recursively_check_substrings(left, right, 1)
 
-    # ^^^^^ MAX STACK DEPTH EXCEEDED
+    # Failed to Timeout
+
+    # Failed due to TIMEOUT ----------
+    # Helper function to check for palindromes
+    # def _is_palindrome(substring):
+    #     if len(substring) < 2: return True
+
+    #     half_length = len(substring)//2 + 1
+    #     for idx in range(half_length):
+    #         if substring[idx] != substring[-1 - idx]:
+    #             return False
+        
+    #     return True
+
+    # longest = s[0]
+    # for left in range(len(s)):
+    #     for right in range(1, len(s)+1):
+    #         substring = s[left:right]
+    #         if _is_palindrome(substring) and right - left > len(longest):
+    #             longest = substring
+
+    # return longest
+
+# bfs search for palindrome
+# start with queue consisting of string s
+# popleft from queue and that is the current substring
+# check if we have visited substring already, if so not a palindrome
+# if not, check if substring is palindrome
+# if it is, it is the longest possible palindrome, so return it
+# if not:
+# add substring without right character/left character to the queue
+from collections import deque
+
+def longestPalindrome(self, s):
+    """
+    :type s: str
+    :rtype: str
+    """
+
+    def is_palindrome(substring):
+        if len(substring) < 2: return True
+
+        half_length = len(substring)//2 + 1
+        for idx in range(half_length):
+            if substring[idx] != substring[-1 - idx]:
+                return False
+        
+        return True
+
+    visited = set()
+    queue = deque([s])
+    
+    def substring_bfs(queue):
+        while len(queue) > 0:
+            substring = queue.popleft()
+
+            # if we already visited don't check
+            if substring in visited: continue
+
+            # the first palindrome we visit will be the largest
+            if is_palindrome(substring):
+                return substring
+
+            # add substring to visited set so we don't check for palindrome again
+            visited.add(substring)
+
+            # add left and right substrings to the queue
+            left_substring = substring[:-1]
+            queue.append(left_substring)
+            right_substring = substring[1:]
+            queue.append(right_substring)
+
+    return substring_bfs(queue)
