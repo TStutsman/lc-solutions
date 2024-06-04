708cfe42373306e464cd39dc8f8eb4c1b956da8d
TStutsman <teagandstutsman@gmail.com>
2024-06-04 14:04:32 -0400

    add: alt two_sum solution

--- two_sum.py
+++ two_sum.py
@@ -35,3 +35,45 @@ class Solution(object):
                 num1_idx = num_indeces[str(num)] # 3
                 num2_idx = num_indeces[str(target - num)] # 5
                 return [num1_idx, num2_idx] # [3, 5]
+            
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # brute force O(n^2)
+        # iterate over all integers i:
+            # iterate over all integers after i (j):
+                # if numi + numj == target, return i and j
+
+        # 2 O(nlogn)
+        # sort integers
+        # iterate over all indeces i:
+            # set current target = target - i
+            # -- binary search for complement --
+            # set pointers left = i, right = len(nums)
+            # choose median number n at index (right - left//2)
+            # if target - n is less than n set right equal to median
+
+        # 3 O(n) time complexity && O(n) space complexity
+        # add every num into a hash { num: idx } O(n)
+        # set first_half_index equal to -1
+        # for every index i O(n)
+            # check if nums[i] is exactly 1/2 of target (duplicate needed)
+                # if first_half_index > -1 return first_half_index, i
+                # else set first_half_index equal to i
+            # if not check if the complement of nums[i] exists in the hash
+                # if so return i, hash[complement]
+
+        index_of = {}
+        first_half_index = -1
+        for i, num in enumerate(nums):
+            index_of[num] = i
+        
+        for i, num in enumerate(nums):
+            if num * 2 == target:
+                if first_half_index > -1:
+                    return [first_half_index, i]
+                else:
+                    first_half_index = i
+            else:
+                complement = target - num
+                if complement in index_of:
+                    return [i, index_of[complement]]
