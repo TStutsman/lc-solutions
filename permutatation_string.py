58f484a7a43398e7114cbaa3bad29a713a904b75
TStutsman <teagandstutsman@gmail.com>
2024-08-07 12:24:00 -0500

    perm string solution 2

--- permutatation_string.py
+++ permutatation_string.py
@@ -25,7 +25,62 @@
 
 #         return False
 
+# class Solution:
+#     def checkInclusion(self, s1: str, s2: str) -> bool:
+#         # make counts hash of s1
+#         # left, right pointers for window in s2
+#         # if counts of s2 matches counts of s1 return true
+#         # increment left and right
+#         # add char at right pointer, remove char from left pointer
+#         if len(s1) > len(s2):
+#             return False
+
+#         char_count = {}
+#         for char in s1:
+#             char_count[char] = char_count.get(char, 0) + 1
+        
+#         for idx in range(len(s1)):
+#             if char_count.get(s2[idx]) == 1:
+#                 del char_count[s2[idx]]
+#             else:
+#                 char_count[s2[idx]] = char_count.get(s2[idx], 0) - 1
+        
+#         r = len(s1)
+#         for l in range(len(s2) - len(s1)):
+#             print(char_count)
+#             if len(char_count) < 1:
+#                 return True
+
+#             if char_count.get(s2[l]) == -1:
+#                 del char_count[s2[l]]
+#             else:
+#                 char_count[s2[l]] = char_count.get(s2[l], 0) + 1
+
+#             if char_count.get(s2[r]) == 1:
+#                 del char_count[s2[r]]
+#             else:
+#                 char_count[s2[r]] = char_count.get(s2[r], 0) - 1
+            
+#             r += 1
+        
+#         if len(char_count) < 1:
+#                 return True
+        
+#         return False
+
 class Solution:
+    def remove_left(self, char_count, left):
+        if char_count.get(left) == -1:
+                del char_count[left]
+        else:
+            char_count[left] = char_count.get(left, 0) + 1
+
+    def add_right(self, char_count, right):
+        if char_count.get(right) == 1:
+                del char_count[right]
+        else:
+            char_count[right] = char_count.get(right, 0) - 1
+
     def checkInclusion(self, s1: str, s2: str) -> bool:
         # make counts hash of s1
         # left, right pointers for window in s2
@@ -39,31 +94,20 @@ class Solution:
         for char in s1:
             char_count[char] = char_count.get(char, 0) + 1
         
-        for idx in range(len(s1)):
-            if char_count.get(s2[idx]) == 1:
-                del char_count[s2[idx]]
-            else:
-                char_count[s2[idx]] = char_count.get(s2[idx], 0) - 1
-        
+        for r in range(len(s1)):
+            self.add_right(char_count, s2[r])
+            
+        if len(char_count) < 1:
+                return True
+
         r = len(s1)
         for l in range(len(s2) - len(s1)):
-            print(char_count)
+            self.remove_left(char_count, s2[l])
+            self.add_right(char_count, s2[r])
+            
             if len(char_count) < 1:
                 return True
 
-            if char_count.get(s2[l]) == -1:
-                del char_count[s2[l]]
-            else:
-                char_count[s2[l]] = char_count.get(s2[l], 0) + 1
-
-            if char_count.get(s2[r]) == 1:
-                del char_count[s2[r]]
-            else:
-                char_count[s2[r]] = char_count.get(s2[r], 0) - 1
-            
             r += 1
-        
-        if len(char_count) < 1:
-                return True
         
         return False
