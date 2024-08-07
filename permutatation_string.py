39a936d1c9b4a4adc307c8bcd77707fa3fc58350
TStutsman <teagandstutsman@gmail.com>
2024-08-07 12:08:55 -0500

    perm string solution 1

--- permutatation_string.py
+++ permutatation_string.py
@@ -1,26 +1,69 @@
+# class Solution:
+#     def checkInclusion(self, s1: str, s2: str) -> bool:
+#         # for s1, count all the characters
+#         # store counts in a hash -> char: count
+#         # for s2, subtract each char from count in hash
+#         # if count is zero delete key
+#         # if no keys remain, return true
+#         # if keys remain after s2 return false
+
+#         counts = {}
+#         for c in s1:
+#             counts[c] = counts.get(c, 0) + 1
+        
+#         for c in s2:
+#             if c not in counts:
+#                 continue
+
+#             if counts[c] < 2:
+#                 del counts[c]
+#             else:
+#                 counts[c] -= 1
+
+#             if len(counts) < 1:
+#                 return True
+
+#         return False
+
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        # for s1, count all the characters
-        # store counts in a hash -> char: count
-        # for s2, subtract each char from count in hash
-        # if count is zero delete key
-        # if no keys remain, return true
-        # if keys remain after s2 return false
+        # make counts hash of s1
+        # left, right pointers for window in s2
+        # if counts of s2 matches counts of s1 return true
+        # increment left and right
+        # add char at right pointer, remove char from left pointer
+        if len(s1) > len(s2):
+            return False
 
-        counts = {}
-        for c in s1:
-            counts[c] = counts.get(c, 0) + 1
+        char_count = {}
+        for char in s1:
+            char_count[char] = char_count.get(char, 0) + 1
         
-        for c in s2:
-            if c not in counts:
-                continue
-
-            if counts[c] < 2:
-                del counts[c]
+        for idx in range(len(s1)):
+            if char_count.get(s2[idx]) == 1:
+                del char_count[s2[idx]]
             else:
-                counts[c] -= 1
-
-            if len(counts) < 1:
+                char_count[s2[idx]] = char_count.get(s2[idx], 0) - 1
+        
+        r = len(s1)
+        for l in range(len(s2) - len(s1)):
+            print(char_count)
+            if len(char_count) < 1:
                 return True
 
+            if char_count.get(s2[l]) == -1:
+                del char_count[s2[l]]
+            else:
+                char_count[s2[l]] = char_count.get(s2[l], 0) + 1
+
+            if char_count.get(s2[r]) == 1:
+                del char_count[s2[r]]
+            else:
+                char_count[s2[r]] = char_count.get(s2[r], 0) - 1
+            
+            r += 1
+        
+        if len(char_count) < 1:
+                return True
+        
         return False
