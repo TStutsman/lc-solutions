ce1dd25d9494d57d4a30125f6c7cb31ad819aa91
TStutsman <teagandstutsman@gmail.com>
2024-08-08 10:58:58 -0500

    merge_strings solution 2

--- merge_strings.py
+++ merge_strings.py
@@ -1,3 +1,22 @@
+# class Solution(object):
+#     def mergeAlternately(self, word1, word2):
+#         """
+#         :type word1: str
+#         :type word2: str
+#         :rtype: str
+#         """
+#         merge_tuples = list(zip(word1, word2))
+#         print(merge_tuples)
+#         list_format = "".join(list(sum(merge_tuples, ())))
+
+#         remaining = ""
+#         if len(word1) > len(word2):
+#             remaining = word1[len(word2):]
+#         elif len(word2) > len(word1):
+#             remaining = word2[len(word1):]
+
+#         return list_format + remaining
+
 class Solution(object):
     def mergeAlternately(self, word1, word2):
         """
@@ -5,14 +24,18 @@ class Solution(object):
         :type word2: str
         :rtype: str
         """
-        merge_tuples = list(zip(word1, word2))
-        print(merge_tuples)
-        list_format = "".join(list(sum(merge_tuples, ())))
+        # iterate through the longest of the two
+        # at each index, add character from word1, then character from word2
+        # if no word1, skip, and viceversa
+        # O(n) where n is the number of characters in the longer string
 
-        remaining = ""
-        if len(word1) > len(word2):
-            remaining = word1[len(word2):]
-        elif len(word2) > len(word1):
-            remaining = word2[len(word1):]
+        longest = max(len(word1), len(word2))
 
-        return list_format + remaining
+        merged = ''
+        for i in range(longest):
+            if i < len(word1):
+                merged += word1[i]
+            if i < len(word2):
+                merged += word2[i]
+
+        return merged
