9ff13387ae3bc7bb6a9fb1564accc8fbdd59af2c
TStutsman <teagandstutsman@gmail.com>
2024-09-13 13:13:22 -0500

    bananas solution

--- eating_bananas.py
+++ eating_bananas.py
@@ -27,3 +27,36 @@
 #                 check += ceil(pile/k)
         
 #         return k
+
+import math
+class Solution:
+    def minEatingSpeed(self, piles: list[int], h: int) -> int:
+        # if h is length of piles then k is max of piles
+        # start with k = min O(n)
+        def hours_to_eat(piles: list[int], rate: int):
+            hours = 0
+            for pile in piles:
+                hours += math.ceil(pile / rate)
+            
+            return hours
+        
+        # ceil(pile / k )= pile-hours
+        # keep doubling k until we exceed target k (hours === h)
+        prev, curr = 0, min(piles)
+        hours = h+1
+        while hours_to_eat(piles, curr) > h:
+            prev = curr
+            curr *= 2
+        
+        l, r = prev, curr
+        while l < r:
+            k = (l + r) // 2
+            if k == 0:
+                return 1
+            hours = hours_to_eat(piles, k)
+            if hours > h:
+                l = k + 1
+            else:
+                r = k
+        
+        return l
