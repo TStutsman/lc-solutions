6a0de8fe71efbae2e0bb1bb5e048e955d3389403
TStutsman <teagandstutsman@gmail.com>
2024-09-20 08:05:32 -0500

    min rotate best solution IMO

--- min_rotate_sorted.py
+++ min_rotate_sorted.py
@@ -1,25 +1,46 @@
+# class Solution:
+#     def findMin(self, nums: list[int]) -> int:
+#         # cut the circle in half - circle again
+#         # how to determine which half to remove?
+#         # the half with the min number will have
+#         # split array in half
+#         # mult a[0] * a[-1] vs b[0] * b[-1]
+#         # smaller value contains min
+#         l, r = 0, len(nums) - 1
+#         while l < r:
+#             m = (l + r) // 2
+#             a = nums[l] * nums[m]
+#             b = nums[m + 1] * nums[r]
+
+#             print(m, a, b)
+
+#             if a < b:
+#                 r = m
+#             elif b < a:
+#                 l = m + 1
+#             else:
+#                 print("ERROR")
+#                 return
+        
+#         return nums[l]
+
 class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        # cut the circle in half - circle again
-        # how to determine which half to remove?
-        # the half with the min number will have
-        # split array in half
-        # mult a[0] * a[-1] vs b[0] * b[-1]
-        # smaller value contains min
+    def findMin(self, nums: list[int]) -> int:
+        # left, right, mid pointers
+        
         l, r = 0, len(nums) - 1
         while l < r:
+            # calculate midpoint
             m = (l + r) // 2
-            a = nums[l] * nums[m]
-            b = nums[m + 1] * nums[r]
 
-            print(m, a, b)
-
-            if a < b:
+            if nums[l] > nums[m]:
+                # the left subarray contains min
                 r = m
-            elif b < a:
+            elif nums[m] > nums[r]:
+                # the right subarray contains min
                 l = m + 1
             else:
-                print("ERROR")
-                return
+                # if nums[r] > nums[l], l points to min
+                break
         
         return nums[l]
