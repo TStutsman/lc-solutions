aaff1cfd4d4f99fef2135de2d158355091a898f9
TStutsman <teagandstutsman@gmail.com>
2025-02-01 16:01:36 -0600

    word search first draft

---
+++ word_search.py
@@ -0,0 +1,44 @@
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        def get_neighbors(x, y) -> List[tuple[int, int]]:
+            res = []
+
+            if x > 0:
+                res.append((x - 1, y))
+            
+            if x < len(board) - 1:
+                res.append((x + 1, y))
+
+            if y > 0:
+                res.append((x, y - 1))
+
+            if y < len(board[0]) - 1:
+                res.append((x, y + 1))
+            
+            return res
+        
+        def dfs(root: tuple[int, int], visited: set[tuple[int, int]]) -> bool:
+            x, y = root
+            if board[x][y] == word[len(visited)]:
+                if len(visited) == len(word) - 1:
+                    return True
+
+                visited.add(root)
+
+                neighbors = get_neighbors(x, y)
+                for neighbor in neighbors:
+                    if neighbor in visited:
+                        continue
+                    if dfs(neighbor, visited):
+                        return True
+
+                visited.discard(root)
+            
+            return False
+        
+        for row in range(len(board)):
+            for col in range(len(board)):
+                if dfs((row, col), set()):
+                    return True
+        
+        return False
