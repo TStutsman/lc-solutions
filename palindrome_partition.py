0b01ff2ec49e95f5e7a9dd347ccd8907db65ccd8
TStutsman <teagandstutsman@gmail.com>
2025-02-01 22:41:05 -0600

    palindrome partition comments

--- palindrome_partition.py
+++ palindrome_partition.py
@@ -1,5 +1,10 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        res = [list(s)]
+        # Start with list of characters in string
+	res = [list(s)]
+	
+	# From each character, find palindromes
+	for char in s:
+		# dfs a palindrome?
 
         return res
