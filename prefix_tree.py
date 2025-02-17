456d1f97af2d7e096bcf54c9a76250883af63ba3
TStutsman <teagandstutsman@gmail.com>
2025-02-17 15:52:54 -0600

    prefix tree first draft

---
+++ prefix_tree.py
@@ -0,0 +1,44 @@
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.next = {}
+
+class PrefixTree:
+    # O(n) space
+    def __init__(self):
+        self.root = TreeNode(None)
+
+    # O(n) insert
+    def insert(self, word: str) -> None:
+        curr = self.root
+        for letter in word:
+            if letter not in curr.next:
+                curr.next[letter] = TreeNode(letter)
+            
+            curr = curr.next[letter]
+
+        curr.next['tail'] = None
+
+
+    # O(n) search
+    def search(self, word: str) -> bool:
+        curr = self.root
+        for letter in word:
+            if letter not in curr.next:
+                return False
+            curr = curr.next[letter]
+        
+        if 'tail' not in curr.next:
+            return False
+        
+        return True
+        
+    # O(n) lookup
+    def startsWith(self, prefix: str) -> bool:
+        curr = self.root
+        for letter in prefix:
+            if letter not in curr.next:
+                return False
+            curr = curr.next[letter]
+        
+        return True
