d6657abd167080fe1395cb1414846bd4cffad9c8
TStutsman <teagandstutsman@gmail.com>
2025-02-24 12:03:52 -0600

    clone graph second solution

--- clone_graph.py
+++ clone_graph.py
@@ -6,26 +6,45 @@ class Node:
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
+# class Solution:
+#     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+#         node_copies = {}
+
+#         def dfs(node:Optional['Node']) -> Optional['Node']:
+#             if not node:
+#                 return None
+
+#             copy = Node(node.val, [])
+#             node_copies[node] = copy
+
+#             for neighbor in node.neighbors:
+#                 if neighbor in node_copies:
+#                     copy.neighbors.append(node_copies[neighbor])
+#                 else:
+#                     nb_copy = dfs(neighbor)
+#                     copy.neighbors.append(nb_copy)
+
+#             return copy
+        
+#         copy = dfs(node)
+
+#         return copy
+
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
         node_copies = {}
 
         def dfs(node:Optional['Node']) -> Optional['Node']:
-            if not node:
-                return None
+            if node in node_copies:
+                return node_copies[node]
 
             copy = Node(node.val, [])
             node_copies[node] = copy
 
             for neighbor in node.neighbors:
-                if neighbor in node_copies:
-                    copy.neighbors.append(node_copies[neighbor])
-                else:
-                    nb_copy = dfs(neighbor)
-                    copy.neighbors.append(nb_copy)
+                nb_copy = dfs(neighbor)
+                copy.neighbors.append(nb_copy)
 
             return copy
-        
-        copy = dfs(node)
 
-        return copy
+        return dfs(node) if node else None
