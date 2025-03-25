671a5aeaf1b4b090a60b8d645cad0477b182894e
TStutsman <teagandstutsman@gmail.com>
2025-03-25 12:02:15 -0500

    non-passing alien dict rough draft

--- Advanced Graphs/alien_dictionary.py
+++ Advanced Graphs/alien_dictionary.py
@@ -21,16 +21,39 @@ class Solution:
             prev = None
             sameLetter = []
             for word in similar:
+                if idx >= len(word):
+                    continue
+
                 if prev and prev != word[idx]:
                     less_than[word[idx]].append(prev)
                     dfs(sameLetter, idx+1)
                     sameLetter = []
+
                 prev = word[idx]
                 sameLetter.append(word)
+
+            if sameLetter:
+                if prev not in less_than:
+                    less_than[prev] = []
+                dfs(sameLetter, idx+1)
         
         dfs(words, 0)
 
         visit = set()
-        for let, lessers in less_than:
+        res = []
+        def add_to_res(letter: str) -> None:
+            if letter in less_than:
+                for lesser in less_than[letter]:
+                    if lesser not in visit:
+                        visit.add(lesser)
+                        add_to_res(lesser)
+            res.append(letter)
+
+
+        for let in less_than:
             if let in visit:
-                continue   
+                continue
+            visit.add(let)
+            add_to_res(let)
+
+        return "".join(res) 
