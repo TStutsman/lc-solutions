3d6fc269b4965b9b4e358e6fd61fc676bae2ed49
TStutsman <teagandstutsman@gmail.com>
2025-04-06 11:44:27 -0500

    longest palindrome refactor solution

--- 1-D Dynamic Programming/longest_palindrome_substring.py
+++ 1-D Dynamic Programming/longest_palindrome_substring.py
@@ -1,24 +1,14 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        def helper(idx: int) -> str:
-            i, j = idx, idx
+        def helper(i: int, j: int) -> str:
             while 0 < i and j < len(s) - 1 and s[i-1] == s[j+1]:
                 i -= 1
                 j += 1
-            
-            pal = s[i:j+1]
 
-            i, j = idx+1, idx
-            while 0 < i and j < len(s) - 1 and s[i-1] == s[j+1]:
-                i -= 1
-                j += 1
-            
-            pal2 = s[i:j+1]
-
-            return max(pal, pal2, key=len)
+            return s[i:j+1]
         
         res = ""
         for i in range(len(s)):
-            res = max(res, helper(i), key=len)
+            res = max(res, helper(i, i), helper(i+1, i), key=len)
         
         return res
