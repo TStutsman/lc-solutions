1c6e5074274020ccc8512b6e9a9ff1f8f3f6ece4
TStutsman <teagandstutsman@gmail.com>
2025-04-05 19:29:01 -0500

    longest palindrome no dp

--- 1-D Dynamic Programming/longest_palindrome_substring.py
+++ 1-D Dynamic Programming/longest_palindrome_substring.py
@@ -1,3 +1,24 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        pass
+        def helper(idx: int) -> str:
+            i, j = idx, idx
+            while 0 < i and j < len(s) - 1 and s[i-1] == s[j+1]:
+                i -= 1
+                j += 1
+            
+            pal = s[i:j+1]
+
+            i, j = idx+1, idx
+            while 0 < i and j < len(s) - 1 and s[i-1] == s[j+1]:
+                i -= 1
+                j += 1
+            
+            pal2 = s[i:j+1]
+
+            return max(pal, pal2, key=len)
+        
+        res = ""
+        for i in range(len(s)):
+            res = max(res, helper(i), key=len)
+        
+        return res
